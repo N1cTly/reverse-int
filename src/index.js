39ee4a6843663fffc86f5df9784867ef3d5c35f1
Tim <tim.kochatkov@gmail.com>
2021-12-15 00:04:46 +0300

    feat: done the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let result = '';
+  while (n !== 0){
+    result = `${result}` + Math.trunc(n % 10);
+    n = Math.trunc(n / 10);
+  }
+  return Number(result);
 }
